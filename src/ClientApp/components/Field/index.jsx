6de9b57c52570bfada25869716cc01c9a9ad837a
Svetlana <shvetsovasvev@gmail.com>
2019-02-23 15:10:21 +0500

    "dont work"

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -10,23 +10,34 @@ export default class Field extends React.Component {
         };
     }
 
-    //const getElementByType = function (type, i) {
-    //    switch (type) {
-    //    case 0:
-    //        return (<div className={styles.block} key={i} />);
-    //    case 1:
-    //        return (<img className={styles.block} key={i} src={iceWall} alt='IceWall' />);
-    //    case 2:
-    //        return (<img className={styles.block} key={i} src={star} alt="Star" />);
-    //    case 3:
-    //        return (<img className={styles.block} key={i} src={finish} alt="Finish" />);
-    //    }
-    //};
 
-    render () {
+    getElementByType = function(type, i) {
+        switch (type) {
+        case 0:
+            return (<td key={i}/>);
+        case 1:
+                return (<td className={styles.box} key={i}/>);
+        case 2:
+                return (<td className={styles.wall} key={i}/>);
+        case 3:
+                return (<td className={styles.target} key={i}/>);
+        case 4:
+            return (<div className="movablePart player" key={i}/>);
+        case 5:
+            return (<div className="movablePart boxOnTarget" key={i}/>);
+        }
+    }
+
+    render() {
         return (
             <div className={styles.root}>
-            </div>
-        );
+                <div className={styles.fieldWrapper}>
+                    <table className={styles.field}>
+                        <tbody>
+                            {this.state.map.map((raw, i) => <tr key={i}>{(raw.map(this.getElementByType))}</tr>)}
+                        </tbody>
+                    </table>
+                </div>
+            </div>);
     }
-}
+};
